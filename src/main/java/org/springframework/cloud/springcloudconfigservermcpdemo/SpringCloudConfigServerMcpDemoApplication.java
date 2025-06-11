4017e2263be21b6cc69fd20f0f97a37cb509dbd0
Ryan Baxter <ryan.baxter@broadcom.com>
2025-06-11 11:32:04 -0400

    Updates

---
+++ src/main/java/org/springframework/cloud/springcloudconfigservermcpdemo/SpringCloudConfigServerMcpDemoApplication.java
@@ -0,0 +1,15 @@
+package org.springframework.cloud.springcloudconfigservermcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerMcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerMcpDemoApplication.class, args);
+	}
+
+}
--- src/test/java/org/springframework/cloud/springcloudconfigservermcpdemo/SpringCloudConfigServerMcpDemoApplicationTests.java
+++ src/test/java/org/springframework/cloud/springcloudconfigservermcpdemo/SpringCloudConfigServerMcpDemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloudConfgServerMcpDemoApplicationTests {
+class SpringCloudConfigServerMcpDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
